d8bca986bbaa282022604c5994a20fa68b73308e
zx <m-zubairahmed@protonmail.com>
2019-04-17 16:07:17 +0530

    completed website

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,19 +1,61 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react'
+import { withKnobs, number } from '@storybook/addon-knobs'
 
-import { Button, Welcome } from '@storybook/react/demo';
+import TopBarLoader from '../src'
+import Introduction from '../demo/introduction'
+import '../demo/styles.css'
+import '../demo/minigram.css'
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+const styles = {
+  textAlign: 'center',
+}
+const CenterDecorator = storyFn => <div style={styles}>{storyFn()}</div>
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+storiesOf('Top bar loader for React', module).add('Introduction', () => (
+  <Introduction />
+))
+
+storiesOf('Demo', module)
+  .addDecorator(withKnobs)
+  .add('Beginning from 0%', () => <TopBarLoader goToPercentage={1} />)
+  .add('Beginning from 50%', () => (
+    <TopBarLoader goToPercentage={50} incrementInterval={500} />
+  ))
+  .add('Changing starting %', () => {
+    const percentageLabel = 'Starting percentage (goToPercentage)'
+    const percentageDefaultValue = 1
+    const percentageOptions = {
+      range: true,
+      min: 1,
+      max: 99,
+      step: 1,
+    }
+    const goToPercentage = number(
+      percentageLabel,
+      percentageDefaultValue,
+      percentageOptions
+    )
+
+    return <TopBarLoader goToPercentage={goToPercentage} />
+  })
+  .add('Changing interval time', () => {
+    const incrementalLabel = 'Control incremental speed (incrementInterval)'
+    const incrementalDefaultValue = 500
+    const incrementalOptions = {
+      range: true,
+      min: 5,
+      max: 5000,
+      step: 5,
+    }
+    const incrementInterval = number(
+      incrementalLabel,
+      incrementalDefaultValue,
+      incrementalOptions
+    )
+
+    return (
+      <TopBarLoader goToPercentage={2} incrementInterval={incrementInterval} />
+    )
+  })
